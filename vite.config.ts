ca6d76b0b33b91c7f04cbb5123ee4209a46f4f04
Biw_CS. <Piyawatbiwkai@gmail.com>
2026-02-25 18:46:52 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Piyawat1313.github.io/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
